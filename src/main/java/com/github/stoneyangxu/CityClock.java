785a40836ab053f1598b24bf4ccdc2d840d0aba4
stoneyangxu <stoneyangxu@icloud.com>
2017-08-20 16:36:30 +0800

    add city clock

---
+++ src/main/java/com/github/stoneyangxu/CityClock.java
@@ -0,0 +1,8 @@
+package com.github.stoneyangxu;
+
+public class CityClock extends Clock {
+    @Override
+    public void setLocalTime(int localTime) {
+        super.localTime = localTime;
+    }
+}
